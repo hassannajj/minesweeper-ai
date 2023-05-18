21fce00435ca691bf47aba886978b1576239b5a4
hassan Hayder Al-Najjar <hhalnajj@circinus-46.ics.uci.edu>
2023-05-17 19:44:09 -0700

    finished minimal

--- Minesweeper_Student-master/Minesweeper_Python/src/MyAI.py
+++ Minesweeper_Student-master/Minesweeper_Python/src/MyAI.py
@@ -18,23 +18,114 @@ from Action import Action
 
 class MyAI( AI ):
 
-	def __init__(self, rowDimension, colDimension, totalMines, startX, startY):
+    def __init__(self, rowDimension, colDimension, totalMines, startX, startY):
 
-		########################################################################
-		#							YOUR CODE BEGINS						   #
-		########################################################################
-		pass
-		########################################################################
-		#							YOUR CODE ENDS							   #
-		########################################################################
+        ########################################################################
+        #							YOUR CODE BEGINS						   #
+        ########################################################################
+        self.rowDimension = rowDimension
+        self.colDimension = colDimension
+        self.totalMines = totalMines
+        self.startX = startX
+        self.startY = startY
+        
+        
+        self.tileCount = 0
+        self.lastX = 0
+        self.lastY = 0
+        self.unmarkedNeighbors = set()
+        self.marked = set()
+        self.unmarked ={(0, 0), (0, 1), (0, 2), (0, 3), (0, 4),
+                        (1, 0), (1, 1), (1, 2), (1, 3), (1, 4),
+                        (2, 0), (2, 1), (2, 2), (2, 3), (2, 4),
+                        (3, 0), (3, 1), (3, 2), (3, 3), (3, 4),
+                        (4, 0), (4, 1), (4, 2), (4, 3), (4, 4)
+                        }
+        
+        
+        
 
-		
-	def getAction(self, number: int) -> "Action Object":
+        ########################################################################
+        #							YOUR CODE ENDS							   #
+        ########################################################################
 
-		########################################################################
-		#							YOUR CODE BEGINS						   #
-		########################################################################
-		return Action(AI.Action.LEAVE)
-		########################################################################
-		#							YOUR CODE ENDS							   #
-		########################################################################
+    
+
+    def getAction(self, number: int) -> "Action Object":
+
+        ########################################################################
+        #							YOUR CODE BEGINS						   #
+        ########################################################################
+        
+        if self.tileCount == 0:
+            x = self.startX
+            y = self.startY
+            
+            self.mark(x, y)
+            self.tileCount += 1
+            return Action(AI.Action.UNCOVER, x, y)
+        
+        
+        
+        elif self.tileCount == 1:
+            self.fillUnmarkedNeighbors(self.startX, self.startY)
+            x, y = self.unmarkedNeighbors.pop()
+            
+            
+            self.mark(x, y)
+            self.tileCount += 1     
+            return Action(AI.Action.UNCOVER, x, y)
+        
+        else:
+            if self.tileCount == 24:
+                return Action(AI.Action.LEAVE)
+            
+            if number == 0:
+                self.fillUnmarkedNeighbors(self.lastX, self.lastY)
+            else:
+                pass
+                # ?
+            
+            
+            # maybe check if there is unmarked neighbors
+            if self.unmarkedNeighbors:
+                
+                x, y = self.unmarkedNeighbors.pop()
+            else:
+                x, y = list(self.unmarked)[0] # get random from unmarked
+                        
+            self.mark(x, y)
+            self.tileCount += 1     
+            return Action(AI.Action.UNCOVER, x, y)
+
+
+
+    def fillUnmarkedNeighbors(self, x, y):
+        # Will append VALID tiles around a tile with a '0' label
+        for dx in [-1, 0, 1]:
+            for dy in [-1, 0, 1]:
+                newX = x + dx
+                newY = y + dy
+                if self.isValidCoor(newX, newY) \
+                and (newX, newY) not in self.unmarkedNeighbors \
+                and (newX, newY) not in self.marked:
+                    self.unmarkedNeighbors.add((newX, newY))
+    
+    def isValidCoor(self, x, y):
+        if x < 0 or x >= self.rowDimension:
+            return False
+        if y < 0 or y >= self.colDimension:
+            return False
+        return True
+
+
+    def mark(self, x, y):
+        self.lastX = x
+        self.lastY = y
+        self.unmarked.discard((x, y))
+        self.marked.add((x, y))
+        
+
+        ########################################################################
+        #							YOUR CODE ENDS							   #
+        ###################### #################################################
